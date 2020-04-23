90cea4a9e35191f6a701550ae6d6143a89905a96
Captain.B <bin@fit2cloud.com>
2020-04-23 17:47:25 +0800

    reflect

--- src/main/java/io/metersphere/streaming/report/ReportGeneratorFactory.java
+++ src/main/java/io/metersphere/streaming/report/ReportGeneratorFactory.java
@@ -1,27 +1,29 @@
 package io.metersphere.streaming.report;
 
 import io.metersphere.streaming.commons.utils.LogUtil;
+import io.metersphere.streaming.commons.utils.ReflectUtils;
 import io.metersphere.streaming.report.impl.AbstractReport;
-import org.reflections.Reflections;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class ReportGeneratorFactory {
 
     public static List<AbstractReport> getReportGenerators() {
-        String packageName = ReportGeneratorFactory.class.getPackage().getName();
-        Reflections reflections = new Reflections(packageName);
-        Set<Class<? extends AbstractReport>> subTypes = reflections.getSubTypesOf(AbstractReport.class);
         List<AbstractReport> reportGenerators = new ArrayList<>();
-        subTypes.forEach(s -> {
-            try {
-                reportGenerators.add(s.newInstance());
-            } catch (Exception e) {
-                LogUtil.error(e);
-            }
-        });
+        try {
+            List<Class<?>> classList = ReflectUtils.getAllAssignedClass(AbstractReport.class);
+            classList.forEach(s -> {
+                try {
+                    reportGenerators.add((AbstractReport) s.newInstance());
+                } catch (Exception e) {
+                    LogUtil.error(e);
+                }
+            });
+        } catch (Exception e) {
+            return reportGenerators;
+        }
+
         return reportGenerators;
     }
 }
