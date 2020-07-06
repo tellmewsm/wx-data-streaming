386c98f6a3a7d997b7e8b84d3a5d0bbdff1f8035
shiziyuan9527 <yuhao.li@fit2cloud.com>
2020-07-06 15:07:25 +0800

    请求统计结果优化

--- src/main/java/io/metersphere/streaming/report/impl/RequestStatisticsReport.java
+++ src/main/java/io/metersphere/streaming/report/impl/RequestStatisticsReport.java
@@ -5,6 +5,8 @@ import io.metersphere.streaming.report.base.Statistics;
 import io.metersphere.streaming.report.parse.ResultDataParse;
 import org.apache.jmeter.report.processor.StatisticsSummaryConsumer;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -17,8 +19,20 @@ public class RequestStatisticsReport extends AbstractReport {
 
     @Override
     public void execute() {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
         Map<String, Object> statisticsDataMap = ResultDataParse.getSummaryDataMap(content, new StatisticsSummaryConsumer());
         List<Statistics> statistics = ResultDataParse.summaryMapParsing(statisticsDataMap, Statistics.class);
+
+        statistics.forEach(statistic -> {
+            statistic.setAverage(decimalFormat.format(new BigDecimal(statistic.getAverage())));
+            statistic.setTp90(decimalFormat.format(new BigDecimal(statistic.getTp90())));
+            statistic.setTp95(decimalFormat.format(new BigDecimal(statistic.getTp95())));
+            statistic.setTp99(decimalFormat.format(new BigDecimal(statistic.getTp99())));
+            statistic.setTransactions(decimalFormat.format(new BigDecimal(statistic.getTransactions())));
+            statistic.setReceived(decimalFormat.format(new BigDecimal(statistic.getReceived())));
+            statistic.setSent(decimalFormat.format(new BigDecimal(statistic.getSent())));
+        });
 
         saveResult(reportId, statistics);
     }
