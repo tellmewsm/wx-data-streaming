b1827da5a0581d90935a9d062b9816a88a8a55ae
Captain.B <bin@fit2cloud.com>
2020-07-30 15:23:29 +0800

    fix: 修复测试的bug

--- src/main/java/io/metersphere/streaming/service/TestResultService.java
+++ src/main/java/io/metersphere/streaming/service/TestResultService.java
@@ -137,7 +137,7 @@ public class TestResultService {
     private void generateReport(String reportId, boolean isForce) {
         if (!isForce) {
             LogUtil.info("report generator is running.");
-//            return;
+            return;
         }
         List<AbstractReport> reportGenerators = ReportGeneratorFactory.getReportGenerators();
         LogUtil.info("report generators size: {}", reportGenerators.size());
