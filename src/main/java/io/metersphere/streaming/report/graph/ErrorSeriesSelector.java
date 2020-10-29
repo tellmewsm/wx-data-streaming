a73c6dd6e41bcc842c31fff0702a6651c2206d64
Captain.B <bin@fit2cloud.com>
2020-10-29 13:45:01 +0800

    refactor: 修改采样的 error message

--- src/main/java/io/metersphere/streaming/report/graph/ErrorSeriesSelector.java
+++ src/main/java/io/metersphere/streaming/report/graph/ErrorSeriesSelector.java
@@ -1,20 +1,42 @@
 package io.metersphere.streaming.report.graph;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.jmeter.report.core.Sample;
 import org.apache.jmeter.report.processor.graph.AbstractSeriesSelector;
+import org.apache.jmeter.report.utils.MetricUtils;
+import org.apache.jmeter.samplers.SampleSaveConfiguration;
+import org.apache.jmeter.util.JMeterUtils;
 
 import java.util.Collections;
 
 public class ErrorSeriesSelector extends AbstractSeriesSelector {
-
+    private static final boolean ASSERTION_RESULTS_FAILURE_MESSAGE = JMeterUtils
+            .getPropDefault(
+                    SampleSaveConfiguration.ASSERTION_RESULTS_FAILURE_MESSAGE_PROP,
+                    true);
 
     @Override
     public Iterable<String> select(Sample sample) {
-        if (StringUtils.isBlank(sample.getFailureMessage())) {
-            return Collections.singletonList("OK");
-        }
-        return Collections.singletonList(sample.getFailureMessage());
+        return Collections.singletonList(getErrorKey(sample));
     }
 
+    static String getErrorKey(Sample sample) {
+        String responseCode = sample.getResponseCode();
+        String responseMessage = sample.getResponseMessage();
+        String key = responseCode + (!StringUtils.isEmpty(responseMessage) ?
+                "/" + StringEscapeUtils.escapeJson(StringEscapeUtils.escapeHtml4(responseMessage)) : "");
+        if (MetricUtils.isSuccessCode(responseCode) ||
+                (StringUtils.isEmpty(responseCode) &&
+                        !StringUtils.isEmpty(sample.getFailureMessage()))) {
+            key = MetricUtils.ASSERTION_FAILED;
+            if (ASSERTION_RESULTS_FAILURE_MESSAGE) {
+                String msg = sample.getFailureMessage();
+                if (!StringUtils.isEmpty(msg)) {
+                    key = StringEscapeUtils.escapeJson(StringEscapeUtils.escapeHtml4(msg));
+                }
+            }
+        }
+        return key;
+    }
 }
