8f4627e4fa803d4aeb251568924dad901739404b
Captain.B <bin@fit2cloud.com>
2020-11-10 19:02:27 +0800

    refactor: 修改 error chart 的计算

--- src/main/java/io/metersphere/streaming/report/graph/ErrorCountValueSelector.java
+++ src/main/java/io/metersphere/streaming/report/graph/ErrorCountValueSelector.java
@@ -1,12 +1,19 @@
 package io.metersphere.streaming.report.graph;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.jmeter.report.core.Sample;
 import org.apache.jmeter.report.processor.graph.CountValueSelector;
 import org.apache.jmeter.report.utils.MetricUtils;
+import org.apache.jmeter.samplers.SampleSaveConfiguration;
+import org.apache.jmeter.util.JMeterUtils;
 
 public class ErrorCountValueSelector extends CountValueSelector {
     private static final Double ONE = 1.0d;
     private static final Double ZERO = 0.0d;
+    private static final boolean ASSERTION_RESULTS_FAILURE_MESSAGE = JMeterUtils
+            .getPropDefault(
+                    SampleSaveConfiguration.ASSERTION_RESULTS_FAILURE_MESSAGE_PROP,
+                    true);
 
     public ErrorCountValueSelector(boolean ignoreTransactionController) {
         super(ignoreTransactionController);
@@ -16,6 +23,15 @@ public class ErrorCountValueSelector extends CountValueSelector {
     @Override
     public Double select(String series, Sample sample) {
         if (MetricUtils.isSuccessCode(sample.getResponseCode())) {
+            if (StringUtils.isEmpty(sample.getResponseCode()) &&
+                    !StringUtils.isEmpty(sample.getFailureMessage())) {
+                if (ASSERTION_RESULTS_FAILURE_MESSAGE) {
+                    String msg = sample.getFailureMessage();
+                    if (!StringUtils.isEmpty(msg)) {
+                        return ONE;
+                    }
+                }
+            }
             return ZERO;
         }
         if (isIgnoreTransactionController()) {
