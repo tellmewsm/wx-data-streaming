17a543e6056cf25d74ac7bb448e5ce0e4303873d
Captain.B <bin@fit2cloud.com>
2020-12-30 13:16:56 +0800

    refactor: 使用整数来统计线程数

--- src/main/java/io/metersphere/streaming/report/impl/LoadChartReport.java
+++ src/main/java/io/metersphere/streaming/report/impl/LoadChartReport.java
@@ -7,6 +7,8 @@ import io.metersphere.streaming.report.parse.ResultDataParse;
 import org.apache.jmeter.report.processor.SampleContext;
 import org.apache.jmeter.report.processor.graph.impl.TransactionsPerSecondGraphConsumer;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class LoadChartReport extends AbstractReport {
@@ -17,6 +19,12 @@ public class LoadChartReport extends AbstractReport {
         SampleContext activeThreadMap = sampleContextMap.get(DistributedActiveThreadsGraphConsumer.class.getSimpleName());
         SampleContext hitsMap = sampleContextMap.get(TransactionsPerSecondGraphConsumer.class.getSimpleName());
         List<ChartsData> resultList = ResultDataParse.graphMapParsing(activeThreadMap.getData(), "users", "yAxis");
+        // 使用整数来统计线程数
+        resultList.forEach(cs -> {
+            DecimalFormat decimalFormat = new DecimalFormat("0");
+            cs.setyAxis(new BigDecimal(decimalFormat.format(cs.getyAxis())));
+            System.out.println(cs.getyAxis());
+        });
         List<ChartsData> hitsList = ResultDataParse.graphMapParsing(hitsMap.getData(), "transactions", "yAxis2");
         resultList.addAll(hitsList);
 
