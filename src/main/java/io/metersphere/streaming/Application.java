08fec905f8178b56db81ec712dd5a31ed238d75c
Captain.B <bin@fit2cloud.com>
2021-02-04 16:12:35 +0800

    refactor: 定时清理数据

--- src/main/java/io/metersphere/streaming/Application.java
+++ src/main/java/io/metersphere/streaming/Application.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {QuartzAutoConfiguration.class})
 @EnableConfigurationProperties({
         JmeterReportProperties.class
 })
 @PropertySource(value = {"file:/opt/metersphere/conf/metersphere.properties"}, encoding = "UTF-8", ignoreResourceNotFound = true)
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
